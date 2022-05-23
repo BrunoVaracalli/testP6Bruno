7e287078ec8e7c417be8d38d905c32e50d841fab
BrunoVaracalli <varacalli97@yahoo.it>
2022-05-23 16:30:56 +0200

    aggiunto saluto

--- Main.java
+++ Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ciao Mondo!");
+        System.out.println("--------------------------------------------");
+        System.out.println("Ciao Mondo1!");
+        System.out.println("Ciao Mondo2!");
+        System.out.println("Ciao Mondo3!");
+        System.out.println("Ciao Mondo4!");
+        System.out.println("Ciao Mondo5!");
+
+
+    }
+
+
 }
